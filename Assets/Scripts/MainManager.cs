e79a48bb695e63a868876ffe37c1b6c6bab54eda
HarryUnt <harrycape2015@gmail.com>
2021-07-30 18:54:24 -0500

    initial commit

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,24 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        BestScoreText.text = GameManager.instance.BestScoreText();
+
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -70,6 +73,13 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        if (GameManager.instance.score < m_Points)
+        {
+            GameManager.instance.score = m_Points;
+            GameManager.instance.bestPlayerName = GameManager.instance.playerName;
+            BestScoreText.text = GameManager.instance.BestScoreText();
+        };
+
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
